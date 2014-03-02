4c7630aba2ce22ba4ae06ad79e7c18dbd6a10265
Steve Kondik <shade@chemlab.org>
2014-03-02 14:34:25 -0800

    Revert "Additional ifdefs for VPU target"
    This reverts commit da84647ca8098d172a1cc99069985b3e4913cb7d.
    
    Change-Id: Ibbc39db5d2cb2f8f45001af8fed8c36ad722928b

--- libhwcomposer/hwc_utils.cpp
+++ libhwcomposer/hwc_utils.cpp
@@ -1341,11 +1341,9 @@ void setMdpFlags(hwc_layer_1_t *layer,
                 ovutils::OV_MDP_BLEND_FG_PREMULT);
     }
 
-#ifdef VPU_TARGET
     if (layer->flags & HWC_VPU_PIPE) {
         ovutils::setMdpFlags(mdpFlags, ovutils::OV_MDP_VPU_PIPE);
     }
-#endif
 
     if(isYuvBuffer(hnd)) {
         if(isSecureBuffer(hnd)) {
